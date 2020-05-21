584d13908dab0f90a0c7f2bbc37f6c0a697fceda
JuanDiegoMartinez <43170453+JuanDiegoMartinez@users.noreply.github.com>
2020-05-21 15:09:03 +0200

    registro terminado

--- src/ddbb/users/PeticionesUsers.ts
+++ src/ddbb/users/PeticionesUsers.ts
@@ -1,10 +1,10 @@
-import {UserRegister} from "../../models/User";
+import {UserComplete, UserRegister} from "../../models/User";
 import {compruebaEmail, compruebaUser} from "./ComprobacionesUsers";
 import {COLLECTION_USUARIOS} from "../Collections";
 import {auth} from "../../index";
 
 //Comprueba que el alias y email del usuario no estén cogidos
-export const compruebaAliasyEmail = async (alias: string, email: string): Promise<{}> => {
+export const compruebaAliasyEmail = async (alias: string, email: string): Promise<{estaAlias: boolean, estaEmail: boolean}> => {
 
     //Comprueba si el alias del usuario está en la bbdd
     let estaAlias = await compruebaUser(alias);
@@ -15,10 +15,11 @@ export const compruebaAliasyEmail = async (alias: string, email: string): Promis
 }
 
 //Maneja el añadir un nuevo usuario en la bbdd
-export const addUser = async (data: UserRegister): Promise<any> => {
+export const addUser = async (data: UserRegister): Promise<boolean> => {
 
     await auth.createUserWithEmailAndPassword(data.email, data.password);
     data.password = "******";
-    let usuario = await COLLECTION_USUARIOS.doc(data.user).set(data);
-    //Poner los campos que faltan como rutaImagen
+    let user: UserComplete = {...data, rutaImagen: "", favoritos: []};
+    await COLLECTION_USUARIOS.doc(data.user).set(user);
+    return true;
 };
